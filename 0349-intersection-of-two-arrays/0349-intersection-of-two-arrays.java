478956483347c096e1c3400ef5f4b4472fefc7d9
Santhosh Kumar M <santhoshkumarm38560@gmail.com>
2026-09-14 22:25:06 +0530

    Time: 2 ms (97.54%), Space: 44.8 MB (81.01%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,39 +1,20 @@
 class Solution {
-    public int[] sort(int arr[]){
-        for (int i=0;i<arr.length-1;i++){
-            for (int j=0;j<arr.length-i-1;j++){
-                if (arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                }
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set=new HashSet<>();
+        ArrayList<Integer> inter=new ArrayList<>();
+        for (int i=0;i<nums1.length;i++){
+            set.add(nums1[i]);
+        }
+        for (int j=0;j<nums2.length;j++){
+            if (set.contains(nums2[j])){
+                inter.add(nums2[j]);
+                set.remove(nums2[j]);
             }
+        }
+        int arr[]=new int[inter.size()];
+        for (int i=0;i<inter.size();i++){
+            arr[i]=inter.get(i);
         }
         return arr;
-    }
-    public int[] intersection(int[] nums1, int[] nums2) {
-        sort(nums1);
-        sort(nums2);
-        int inter[]=new int[nums1.length+nums2.length];
-        int temp1=0;
-        int temp2=0;
-        int i=0;
-        while(temp1!=nums1.length && temp2!=nums2.length){
-            if (nums1[temp1]<nums2[temp2]){
-                temp1++;
-            }
-            else if (nums2[temp2]<nums1[temp1]){
-                temp2++;
-            }
-            else{
-                if (i==0 || inter[i-1]!=nums1[temp1]){
-                    inter[i]=nums1[temp1];
-                    i++;
-                }
-                temp1++;
-                temp2++;
-            }
-        }
-        return Arrays.copyOf(inter,i);
     }
 }
